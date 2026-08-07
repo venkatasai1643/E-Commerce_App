36c7483d9bf73387a1ed3838364a90693bbb39de
venkatasai1643 <saimaddipatla1919@gmail.com>
2026-08-07 21:32:43 +0530

    Setup

---
+++ src/test/java/com/e_commerce/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
