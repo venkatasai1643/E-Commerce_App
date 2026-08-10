0a2c28164baba6e7e61350c91cb58142b2ae1f8e
Pothunuri Laxmi Sree <pothunurilaxmisree@gmail.com>
2026-08-10 23:45:50 +0530

    Created table users

---
+++ src/main/java/com/e_commerce/application/entity/User.java
@@ -0,0 +1,38 @@
+package com.e_commerce.application.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable=false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true,nullable=false)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @Column(nullable=false)
+    private String role;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+}
