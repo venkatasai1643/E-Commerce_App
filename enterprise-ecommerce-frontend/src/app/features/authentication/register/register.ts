e6c66f98532e23b41927a9fb266f7662a51783d5
venkatasai1643 <saimaddipatla1919@gmail.com>
2026-08-11 18:50:03 +0530

    register

--- enterprise-ecommerce-frontend/src/app/features/authentication/register/register.ts
+++ enterprise-ecommerce-frontend/src/app/features/authentication/register/register.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+
+  selectedRole: String = 'CUSTOMER';
+}
