d520570c4aacbcdfe727dafa29f56c2ce8dd39b4
venkatasai1643 <saimaddipatla1919@gmail.com>
2026-08-13 20:06:15 +0530

    Password encryption

---
+++ src/main/java/com/e_commerce/application/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.e_commerce.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    private String email;
+
+    private String password;
+}
--- src/main/java/com/e_commerce/application/service/AuthServiceImpl.java
+++ src/main/java/com/e_commerce/application/service/AuthServiceImpl.java
@@ -1,6 +1,7 @@
 package com.e_commerce.application.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.e_commerce.application.entity.User;
@@ -12,16 +13,31 @@ public class AuthServiceImpl {
     @Autowired
     private UserRepository repository;
 
-    public void insertUserDetails(User user){
-        repository.save(user);
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User insertUserDetails(User user){
+
+        String hashedPassword =
+                passwordEncoder.encode(user.getPassword());
+
+        user.setPassword(hashedPassword);
+
+        return repository.save(user);
     }
 
     public String loginUser(User user){
         User existingUser = repository.findByEmail(user.getEmail()).orElse(null);
-        if (existingUser != null && existingUser.getPassword().equals(user.getPassword())) {
+        
+         boolean passwordMatches =
+                passwordEncoder.matches(
+                        user.getPassword(),
+                        existingUser.getPassword()
+                );
+         if (passwordMatches) {
             return "Login successful";
-        } else {
-            return "Invalid email or password";
         }
+
+        return "Invalid email or password";
     }
 }
